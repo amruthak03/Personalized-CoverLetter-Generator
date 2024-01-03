dcbbeaa1425a3f159e5675c537fa3099d3f20d13
Amrutha <110037114+amruthak03@users.noreply.github.com>
2024-01-03 15:26:46 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,12 +2,10 @@ from langchain.chains import LLMChain, LLMRequestsChain
 from constant_templates import Job_Description_Template
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-import streamlit as st
 
 # function to scrape the job description and qualifications from the job website for the company url fetched
 def company_scraper(company_url:str):
-    openai_secret = st.secrets.get("openai")
-    openai_api_key = openai_secret.get("key")
+    openai_api_key = 'YOUR_API_KEY'
     llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', openai_api_key=openai_api_key)
 
     prompt = PromptTemplate(input_variables=["requests_result"], template=Job_Description_Template)
